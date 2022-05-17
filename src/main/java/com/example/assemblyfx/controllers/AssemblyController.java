0581ffe9be0f688ed6492c2c59ba3377f235f206
Christian <bantex99@hotmail.com>
2022-05-17 11:05:44 +0200

    Assemblyfront

--- src/main/java/com/example/assemblyfx/controllers/AssemblyController.java
+++ src/main/java/com/example/assemblyfx/controllers/AssemblyController.java
@@ -1,20 +1,34 @@
 package com.example.assemblyfx.controllers;
 
 import MQTT.AssemblyMQTT;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.io.IOException;
 
 public class AssemblyController {
     @FXML
-    private TextArea status;
+    private Button start;
     @FXML
     private Button statusBtn;
-
+    @FXML
+    private TextField input;
+    @FXML
+    private Label lastOP;
+    @FXML
+    private Label currentOP;
+    @FXML
+    private Label state;
+    @FXML
+    private Label time;
+    @FXML
+    private Label health;
     AssemblyMQTT mqtt;
 
     public AssemblyController() throws MqttException {
@@ -23,11 +37,14 @@ public class AssemblyController {
     public void initialize() throws MqttException, IOException, InterruptedException {
         mqtt = AssemblyMQTT.getInstance();
         mqtt.connect();
-        mqtt.publishMessage(654);
+        mqtt.publishMessage(500);
     }
 
-
     public void btnClick(ActionEvent actionEvent) {
-        status.appendText(mqtt.getAssemblyStationState());
+        lastOP.setText(mqtt.getLastOperation());
+        currentOP.setText(mqtt.getCurrentOperation());
+        state.setText(mqtt.getAssemblyStationState());
+        time.setText(mqtt.getAssemblyTimeStamp());
+        health.setText(mqtt.getHealth());
     }
 }
